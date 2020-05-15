856b589e01f773402279824c9d6170f64b001e3a
Richard Kempert <ritchyk@richards-mbp.home>
2020-05-15 14:59:19 +0200

    Wetterstation von Tirol hinzugefügt

--- wetter.js
+++ wetter.js
@@ -15,3 +15,44 @@ let rainviewer = L.control.rainviewer({
     opacity: 0.5
 });
 rainviewer.addTo(map);
+
+let overlay = {
+    stations: L.featureGroup(),
+
+}
+
+L.control.layers({
+    "BasemapAT.grau": L.tileLayer.provider("BasemapAT.grau"),
+    "BasemapAT.terrain": L.tileLayer.provider("BasemapAT.terrain"),
+    "BasemapAT.orthofoto": L.tileLayer.provider("BasemapAT.orthofoto"),
+}, {
+    "Wetterstationen Tirol": overlay.stations,
+}).addTo(map);
+
+// let awsUrl = "https://aws.openweb.cc/stations";
+// let aws = L.geoJson.ajax(awsUrl, {
+//     filter: function (feature) {
+//         return feature.properties.LT /*!== null -->ist unnötig!! */ ;
+
+//     },
+//     pointToLayer: function (point, latlng) {
+//         console.log("point: ", point);
+//         let marker = L.marker(latlng, {
+
+//         });
+//         marker.bindPopup(`<h3>${point.properties.name} ${point.geometry.coordinates[2]} m</h3>
+//         <ul>
+//         <li>Position: Lat: ${point.geometry.coordinates[1]}/Lng: ${point.geometry.coordinates[0]}</li>
+//         <li>Datum: ${point.properties.date}</li>
+//         <li>Lufttemperatur: ${point.properties.LT} °C</li>
+//         <li>Windgeschwindigkeit: ${point.properties.WG} m/s</li>
+//         <li>Relative Luftfeuchte: ${point.properties.RH} %</li>
+//         <li>Schneehöhe: ${point.properties.HS} cm</li>
+//         <li> Grafik: <a href=https://lawine.tirol.gv.at/data/grafiken/1100/standard/tag/${point.properties.plot}.png> ${point.properties.plot} </a></li>
+//         </ul>`);
+//         //console.log("feature in filter", point);
+//         return marker;
+
+//     }
+// }).addTo(overlay.stations);
+
