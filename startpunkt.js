f49b10bb83732f591ccfffef3b4e5a010aa25e9c
Lukas Müller <Lukas.V.Mueller@student.uibk.ac.at>
2020-05-30 12:05:17 +0200

    GeoSearch Test

--- startpunkt.js
+++ startpunkt.js
@@ -1,6 +1,25 @@
-let map = L.map("map", {
-    center: [47.3, 11.5],
-    zoom: 10,
-    layers: [L.tileLayer.provider("BasemapAT.grau")]
+/*
+const searchControl = new GeoSearchControl({
+  provider: "OpenStreetMap.Mapnik",
 });
 
+const map = new L.map('searchMap');
+map.addControl(searchControl);
+*/
+
+let map = L.map("searchMap", {
+    center: [47.3, 11.5],
+    zoom: 10,
+    layers: [L.tileLayer.provider('BasemapAT.grau')]
+});
+
+/*
+map.addControl(searchControl);
+
+new GeoSearchControl({
+    provider: "OpenStreetMap.Mapnik", // required
+    style: 'bar', // optional: bar|button  - default button
+}).addTo(map);
+
+map.addControl(searchControl);
+*/
